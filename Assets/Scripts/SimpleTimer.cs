4324353e28b54943b73514cca0a2a6dedd5f9280
meghnaraswan <raswan@chapman.edu>
2022-04-29 15:41:49 -0700

    adding level 1 design

--- Assets/Scripts/SimpleTimer.cs
+++ Assets/Scripts/SimpleTimer.cs
@@ -5,36 +5,69 @@ using UnityEngine.UI;
 
 public class SimpleTimer : MonoBehaviour
 {
-    public float timeLimit = 20;
-    private float timeGamePlayingStarted;
+    //public float timeLimit = 20;
+    //private float timeGamePlayingStarted;
+    //private GameController gameController;
+
+    //private void Awake()
+    //{
+    //    gameController = GetComponentInParent<GameController>();
+    //}
+
+    //// Start is called before the first frame update
+    //private void Start()
+    //{
+    //    timeGamePlayingStarted = Time.time;
+    //}
+
+    //// Update is called once per frame
+    //private void Update()
+    //{
+    //    float timeSinceGamePlayingStarted = Time.time - timeGamePlayingStarted;
+
+    //    if (timeSinceGamePlayingStarted > timeLimit)
+    //    {
+    //        //Update game state on controller to be game lost
+    //        gameController.StateUpdate(GameController.GameStates.GameLost);
+    //        //Turn off this component, disables functionality so we don't spam the GameController
+    //        this.enabled = false;
+    //    }
+
+    //    //cast time to an int
+    //    int timerCount = (int) timeSinceGamePlayingStarted;
+    //    //Update Timer text on screen
+    //    gameController.UpdateGameTimer(timerCount);
+    //}
+    public float timeLimit = 90;
+    //private float timeGamePlayingStarted;
     private GameController gameController;
 
     private void Awake()
     {
         gameController = GetComponentInParent<GameController>();
     }
-    
+
     // Start is called before the first frame update
     private void Start()
     {
-        timeGamePlayingStarted = Time.time;
+        //timeGamePlayingStarted = 0;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        float timeSinceGamePlayingStarted = Time.time - timeGamePlayingStarted;
+        float timeSinceGamePlayingStarted = timeLimit - Time.time;
 
-        if (timeSinceGamePlayingStarted > timeLimit)
+        if (timeSinceGamePlayingStarted < 0)
         {
             //Update game state on controller to be game lost
             gameController.StateUpdate(GameController.GameStates.GameLost);
             //Turn off this component, disables functionality so we don't spam the GameController
             this.enabled = false;
         }
-        
+
         //cast time to an int
-        int timerCount = (int) timeSinceGamePlayingStarted;
+        int timerCount = (int)timeSinceGamePlayingStarted;
         //Update Timer text on screen
         gameController.UpdateGameTimer(timerCount);
     }
